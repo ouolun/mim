63bcff6b134818d5fb86ebaf140d121d50dfa3c8
ouolun <90953487+ouolun@users.noreply.github.com>
2025-10-28 22:39:55 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'MIM V1.0.0'; 
+const REPO_NAME = '/mim';
+
+const urlsToCache = [
+  `${REPO_NAME}/`,
+  `${REPO_NAME}/index.html`,
+  `${REPO_NAME}/style.css`,
+  `${REPO_NAME}/script.js`,
+  `${REPO_NAME}/202404.json`,
+  `${REPO_NAME}/202411.json`,
+  `${REPO_NAME}/202504.json`,
+  `${REPO_NAME}/apple-touch-icon.png`,
+  `${REPO_NAME}/favicon.png`,
+  `${REPO_NAME}/favicon.ico`,
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('SW:正在下載快取資料...');
+        return cache.addAll(urlsToCache).catch(error => {
+            console.error('SW:警告，部分資源快取失敗:', error);
+        });
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('[SW] Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim(); 
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  }
+});
