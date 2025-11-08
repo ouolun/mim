eab7bc10c1b7596e21b2a6291c3717599192b37f
ouolun <90953487+ouolun@users.noreply.github.com>
2025-11-08 23:57:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/*V1.2.3 Release*/
-const CACHE_NAME = 'MIM V1.2.3 Release'; 
+/*V1.2.4 Release*/
+const CACHE_NAME = 'MIM V1.2.4 Release'; 
 const REPO_NAME = '/mim';
 
 const urlsToCache = [
@@ -8,7 +8,6 @@ const urlsToCache = [
   `${REPO_NAME}/style.css`,
   `${REPO_NAME}/script.js`,
   `${REPO_NAME}/purify.min.js`,
-  `${REPO_NAME}/marked.min.js`,
   `${REPO_NAME}/202404.json`,
   `${REPO_NAME}/202411.json`,
   `${REPO_NAME}/202504.json`,
@@ -67,4 +66,5 @@ self.addEventListener('message', event => {
         self.skipWaiting();
     }
 });
+
 
