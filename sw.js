a4e42818a85c560cabe5015e8a94b8651a4072e4
ouolun <90953487+ouolun@users.noreply.github.com>
2025-11-17 23:43:36 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/*V1.3.1 Release*/
-const CACHE_NAME = 'MIM V1.3.1 Release'; 
+/*V1.3.2 Release*/
+const CACHE_NAME = 'MIM V1.3.2 Release'; 
 const REPO_NAME = '/mim';
 
 const urlsToCache = [
@@ -84,5 +84,6 @@ self.addEventListener('message', event => {
         self.skipWaiting();
     }
 });
+
 
 
